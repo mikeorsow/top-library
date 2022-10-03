fe5311cde37d0f7d118ff28e0c4c5ae2d214a509
Mike Orsow <mike.orsow@gmail.com>
2022-10-02 23:14:56 -0700

    Add ugly button that toggles read status

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Populate a few default library books
+// Generate a few default library books
 const BOOK1 = new Book(
   'Inspired: How to Create Tech Products Customers Love',
   'Marty Cagan',
@@ -15,7 +15,7 @@ const BOOK3 = new Book(
   'Loved: How to Rethink Marketing for Tech Products',
   'Martina Lauchengco',
   276,
-  'yes'
+  'no'
 );
 
 let myLibrary = [BOOK1, BOOK2, BOOK3];
@@ -27,9 +27,12 @@ function Book(title, author, pages, readStatus) {
   this.readStatus = readStatus;
 }
 
-Book.prototype.toggleReadStatus = () => {
-  return console.log('you been toggled')
-}
+Book.prototype.toggleReadStatus = function () {
+  if (this.readStatus === 'no') {
+    return (this.readStatus = 'yes');
+  }
+  return (this.readStatus = 'no');
+};
 
 const bookList = document.getElementById('book-list');
 const newBookForm = document.getElementById('new-book-form');
@@ -45,8 +48,11 @@ function listAllBooks() {
   if (myLibrary.length === 0) {
     return (bookList.innerHTML = '<h2>You have no books!</h2>');
   }
+  // Clear the list of books before reloading to avoid duplicating the list
   bookList.innerHTML = '';
-  return myLibrary.map((book, index) => {
+
+  // Display each book as a card on the page
+  myLibrary.map((book, index) => {
     const bookCard = document.createElement('div');
     bookCard.classList.add('book-card');
     bookCard.setAttribute('data-index', index);
@@ -58,17 +64,31 @@ function listAllBooks() {
           <p class="book-card-pages">${book.pages} pages</p>
           <p class="book-card-status">read: ${book.readStatus}</p>
       </div>
-      <button class="book-card-remove">Remove</button>`;
+      <button class="book-card-toggleReadStatus">Toggle Read Status</button>
+      <button class="book-card-remove">Remove</button>
+      `;
     bookList.appendChild(bookCard);
-
-    const removeBookButtons = document.querySelectorAll(
-      'button.book-card-remove'
-    );
-
-    removeBookButtons.forEach((button) =>
-      button.addEventListener('click', removeBookFromLibrary)
-    );
   });
+
+  // Add click listeners to the Remove buttons on the book cards
+  const removeBookButtons = document.querySelectorAll(
+    'button.book-card-remove'
+  );
+  removeBookButtons.forEach((button) =>
+    button.addEventListener('click', removeBookFromLibrary)
+  );
+
+  // Add click listeners to the Toggle Read Status buttons on the book cards
+  const toggleReadStatusButtons = document.querySelectorAll(
+    'button.book-card-toggleReadStatus'
+  );
+  toggleReadStatusButtons.forEach((button) =>
+    button.addEventListener('click', (e) => {
+      const bookIndex = e.target.parentElement.dataset.index;
+      myLibrary[bookIndex].toggleReadStatus();
+      listAllBooks();
+    })
+  );
 }
 
 function removeBookFromLibrary(e) {
